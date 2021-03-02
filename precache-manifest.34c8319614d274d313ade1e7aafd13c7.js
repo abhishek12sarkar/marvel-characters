1dbcf6fe3513035556d8091dc6882a38e82f50d7
Abhishek Sarkar <abhishek.sarkar@intimetec.com>
2021-03-02 14:57:15 +0530

    Updates

---
+++ precache-manifest.34c8319614d274d313ade1e7aafd13c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5c8afffb8a3adc35bbba4b2f9e3ad8",
+    "url": "/abhishek12sarkar/marvel-characters/index.html"
+  },
+  {
+    "revision": "cc281a6bcf7ed96105e1",
+    "url": "/abhishek12sarkar/marvel-characters/static/css/main.cbf9d98b.chunk.css"
+  },
+  {
+    "revision": "706a8bea77798945aaac",
+    "url": "/abhishek12sarkar/marvel-characters/static/js/2.1d5c5bb9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/abhishek12sarkar/marvel-characters/static/js/2.1d5c5bb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc281a6bcf7ed96105e1",
+    "url": "/abhishek12sarkar/marvel-characters/static/js/main.950b22dc.chunk.js"
+  },
+  {
+    "revision": "05ae17ae7d62393e0232",
+    "url": "/abhishek12sarkar/marvel-characters/static/js/runtime-main.86ee5b87.js"
+  }
+]);
